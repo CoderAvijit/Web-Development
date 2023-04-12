56130abfad28170af626accfb29e71b77fad8c80
CoderAvijit <95471951+CoderAvijit@users.noreply.github.com>
2023-04-12 11:53:01 +0530

    UpdatedVersion
    Updated

--- FlowersBuy/Website/script.js
+++ FlowersBuy/Website/script.js
@@ -1,43 +1,82 @@
-// Get the add-to-cart buttons
-const addToCartButtons = document.querySelectorAll('.add-to-cart');
+const addProductBtn = document.getElementById('add-product-btn');
+const productList = document.getElementById('product-list');
+let length = document.getElementsByClassName("product").length+1;
+var list = new Array();
+addProductBtn.addEventListener('click', function(event) {
+  event.preventDefault();
 
-// Get the cart items and total
+  const productImage = document.getElementById('product-image').files[0];
+  const productName = document.getElementById('product-name').value;
+  const productDetails = document.getElementById('product-details').value;
+  const productPrice = document.getElementById('product-price').value;
+
+  const reader = new FileReader();
+
+  reader.addEventListener('load', function() {
+    const product = createProduct(reader.result, productName,productDetails, productPrice);
+    productList.appendChild(product);
+    list.push(product.id);
+    length+=1;
+  });
+
+  reader.readAsDataURL(productImage);
+});
+
+function createProduct(imageUrl, name, details, price) {
+  const div = document.createElement('div');
+  div.className = 'product';
+  div.setAttribute('id',"product-"+length);
+  const img = document.createElement('img');
+  img.src = imageUrl;
+  img.width = 200;
+  img.alt = name;
+  const h2 = document.createElement('h2');
+  h2.className = 'name';
+  h2.textContent = name;
+  const h3 = document.createElement('h3');
+  h3.textContent = details;
+  const p = document.createElement('p');
+  p.className = 'price';
+  p.textContent = 'Rs.' + price;
+  const button = document.createElement('button');
+  button.className = 'add-to-cart';
+  button.setAttribute('data-price', price);
+  button.textContent = 'Buy';
+  button.addEventListener('click', function() {
+    const productName = this.parentElement.querySelector('.name').textContent;
+    const productPrice = parseFloat(this.getAttribute('data-price'));
+    cart.push({ name: productName, price: productPrice, quantity: 1 });
+    updateCartUI();
+  });
+
+  div.appendChild(img);
+  div.appendChild(h2);
+  div.appendChild(h3);
+  div.appendChild(p);
+  div.appendChild(button);
+
+  return div;
+}
+
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
 const cartItems = document.getElementById('cart-items');
 const cartTotal = document.getElementById('cart-total');
-
-// Initialize the cart
 let cart = [];
-var list = new Array();
-
-// Handle add-to-cart button clicks
 addToCartButtons.forEach(button => {
   button.addEventListener('click', () => {
-    // Get the product name and price
-    const productName = button.previousElementSibling.previousElementSibling.textContent;
+    const productName = button.parentElement.querySelector('.name').textContent;
     list.push(button.parentElement.id);
+    // list.push(addToCartButtons.item);
     const productPrice = parseFloat(button.getAttribute('data-price'));
-
-    // Add the product to the cart
     cart.push({ name: productName, price: productPrice, quantity: 1 });
-
-    // Update the cart UI
     updateCartUI();
   });
 });
-
-// Update the cart UI
 function updateCartUI() {
-  // Clear the cart items
   cartItems.innerHTML = '';
-
-  // Calculate the cart total
   let total = 0;
   cart.forEach(item => {
     total += item.price * item.quantity;
-  });
-
-  // Update the cart items and total
-  cart.forEach(item => {
     const li = document.createElement('li');
     li.innerHTML = `
       <span class="name">${item.name}</span>
@@ -46,24 +85,19 @@ function updateCartUI() {
     `;
     cartItems.appendChild(li);
   });
-  cartTotal.textContent = `RS.${total.toFixed(2)}`;
+  cartTotal.textContent = `Rs.${total.toFixed(2)}`;
 
-  // Handle checkout button click
   const checkoutButton = document.getElementById('checkout-button');
   checkoutButton.addEventListener('click', () => {
-    for(var i=0; i<list.length; i++){
+    // alert(list);
+    for (let i = 0; i < list.length; i++) {
+      length-=1;
       const productId = list[i];
       const productCard = document.getElementById(productId);
       productCard.remove();
     }
-
-    // Clear the list array
     list = [];
-
-    // Clear the cart array
     cart = [];
-
-    // Update the cart UI
     updateCartUI();
   });
 }
