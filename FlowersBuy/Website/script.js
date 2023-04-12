7a5af3340f445445860eeac62bf8e26a0ae87bc2
CoderAvijit <95471951+CoderAvijit@users.noreply.github.com>
2023-04-13 00:29:41 +0530

    updated
    updated

--- FlowersBuy/Website/script.js
+++ FlowersBuy/Website/script.js
@@ -1,10 +1,8 @@
 const addProductBtn = document.getElementById('add-product-btn');
 const productList = document.getElementById('product-list');
-let length = document.getElementsByClassName("product").length+1;
-var list = new Array();
+let length = 19;
 addProductBtn.addEventListener('click', function(event) {
   event.preventDefault();
-
   const productImage = document.getElementById('product-image').files[0];
   const productName = document.getElementById('product-name').value;
   const productDetails = document.getElementById('product-details').value;
@@ -13,19 +11,19 @@ addProductBtn.addEventListener('click', function(event) {
   const reader = new FileReader();
 
   reader.addEventListener('load', function() {
-    const product = createProduct(reader.result, productName,productDetails, productPrice);
+    const idName = "product-"+length;
+    const product = createProduct(reader.result, productName,productDetails, productPrice,idName);
     productList.appendChild(product);
-    list.push(product.id);
     length+=1;
   });
 
   reader.readAsDataURL(productImage);
 });
 
-function createProduct(imageUrl, name, details, price) {
+function createProduct(imageUrl, name, details, price,idName) {
   const div = document.createElement('div');
   div.className = 'product';
-  div.setAttribute('id',"product-"+length);
+  div.setAttribute('id',idName);
   const img = document.createElement('img');
   img.src = imageUrl;
   img.width = 200;
@@ -45,7 +43,8 @@ function createProduct(imageUrl, name, details, price) {
   button.addEventListener('click', function() {
     const productName = this.parentElement.querySelector('.name').textContent;
     const productPrice = parseFloat(this.getAttribute('data-price'));
-    cart.push({ name: productName, price: productPrice, quantity: 1 });
+    const id_Name = this.parentElement.id;
+    cart.push({ name: productName, price: productPrice, quantity: 1,idName: id_Name});
     updateCartUI();
   });
 
@@ -66,9 +65,9 @@ addToCartButtons.forEach(button => {
   button.addEventListener('click', () => {
     const productName = button.parentElement.querySelector('.name').textContent;
     list.push(button.parentElement.id);
-    // list.push(addToCartButtons.item);
+    const id_Name = button.parentElement.id;
     const productPrice = parseFloat(button.getAttribute('data-price'));
-    cart.push({ name: productName, price: productPrice, quantity: 1 });
+    cart.push({ name: productName, price: productPrice, quantity: 1,idName:id_Name });
     updateCartUI();
   });
 });
@@ -81,18 +80,16 @@ function updateCartUI() {
     li.innerHTML = `
       <span class="name">${item.name}</span>
       <span class="quantity">${item.quantity}</span>
+      <span class="Id">${item.idName}</span>
       <span class="price">Rs.${(item.price * item.quantity).toFixed(2)}</span>
     `;
     cartItems.appendChild(li);
   });
   cartTotal.textContent = `Rs.${total.toFixed(2)}`;
-
   const checkoutButton = document.getElementById('checkout-button');
   checkoutButton.addEventListener('click', () => {
-    // alert(list);
-    for (let i = 0; i < list.length; i++) {
-      length-=1;
-      const productId = list[i];
+    for (let i = 0; i < cart.length; i++) {
+      const productId = cart[i].idName;
       const productCard = document.getElementById(productId);
       productCard.remove();
     }
